91a92e9811f60d5fd31fa2d3cd2693fd473ea51d
Brandi <Brandi@Brandis-MacBook-Air.local>
2022-07-31 08:18:16 -0400

    added forms and beginning of JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+document.querySelector('button').addEventListener('click', choose)
+
+function choose(){
+  let choice = Math.ceil( Math.random() * 2 )
+    if (choice == 1){ console.log (opt1)}
+        else{ console.log (opt2) }
+}
