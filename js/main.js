659f48dca21fd21cebfc6c9dc0cf3d28f940f8d8
Brandi <Brandi@Brandis-MacBook-Air.local>
2022-07-31 08:43:34 -0400

    variable pulled from input boxes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ document.querySelector('button').addEventListener('click', choose)
 
 function choose(){
   let choice = Math.ceil( Math.random() * 2 )
-    if (choice == 1){ console.log (opt1)}
-        else{ console.log (opt2) }
+    if (choice == 1){ console.log (document.getElementById('opt1').value)}
+        else{ console.log (document.getElementById('opt2').value) }
 }
